a4acaca7aabebee8ee873edd1ed0618c90b9269a
Francisco Quesada <fquesada@itba.edu.ar>
2022-03-13 20:18:41 -0300

    fix :bug: insensitive string compare

--- TP1/main.py
+++ TP1/main.py
@@ -1,4 +1,4 @@
-from argparse import ArgumentError
+from argparse import ArgumentParser
 import json
 import time
 from search_methods.SearchMethod import SearchMethod 
@@ -8,8 +8,9 @@ from search_methods.constants import BLANK, N
 from search_methods.visualization import plot_tree
 
 def print_initial_data(n,initial_state,search_method,heuristic,initial_limit,max_depth):
-  print(f"Searching solution for grid of size {N} and initial state: {initial_state}")
+  print(f"Searching solution for grid of size {n} and initial state: {initial_state}")
   print(f"Search method: {search_method}")
+
   if heuristic:
     print(f"Performing informed method with heuristic: {heuristic}")
   elif initial_limit:
@@ -19,7 +20,6 @@ def print_initial_data(n,initial_state,search_method,heuristic,initial_limit,max
 
 def main():
 
-  from argparse import ArgumentParser,FileType
   parser = ArgumentParser()
   parser.add_argument("-f", "--file", dest="config_file",
                       help="read configuration file",default='config.json')
@@ -32,7 +32,7 @@ def main():
   except:
     print("Config file not found!")
     exit()
-    
+
   # Read config json
   config = json.load(f)
 
@@ -46,17 +46,17 @@ def main():
   parsed_heuristic = None
   
   try:
-    SearchMethod[search_method]
+    SearchMethod[search_method.upper()]
   except:
     print("Invalid search method")
     exit()
 
   # Load heuristic in case that exists
-  if heuristic == 'manhattan':
+  if heuristic.lower() == 'manhattan':
     parsed_heuristic = Heuristic.MANHATTAN_DISTANCE
-  elif heuristic == 'euclidean':
+  elif heuristic.lower() == 'euclidean':
     parsed_heuristic = Heuristic.EUCLIDEAN_DISTANCE
-  elif heuristic == 'misplaced_tiles':
+  elif heuristic.lower() == 'misplaced_tiles':
     parsed_heuristic = Heuristic.MISPLACED_TILES
 
   # Print search parameters
@@ -64,10 +64,10 @@ def main():
   
   # Perform desired search
   start=time.perf_counter()
-  result = search(initial_state,SearchMethod[search_method], N,parsed_heuristic)
+  result = search(initial_state,SearchMethod[search_method.upper()], N,parsed_heuristic)
   end = time.perf_counter()
   if result is not None:
-    print("Solution Found!")
+    print(f"Solution Found!\n{str(result[0])}")
   print(f"Search completed in {end - start:0.4f} seconds")
 
 
