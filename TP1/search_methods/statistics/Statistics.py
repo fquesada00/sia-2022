53d2fc520d990b1652aab65c38ec15ceefdd7449
Francisco Quesada <fquesada@itba.edu.ar>
2022-03-14 13:06:31 -0300

    refactor :recylce: changed str of classes
    Co-authored-by: Octavio Serpe <oserpe@itba.edu.ar>

--- TP1/search_methods/statistics/Statistics.py
+++ TP1/search_methods/statistics/Statistics.py
@@ -22,13 +22,14 @@ class Statistics:
         return count
 
     def __get_solution_path(self, goal_node):
+        path = []
         current = goal_node
         while current.parent is not None:
-            self.solution_path.insert(0,
-                                      Transition(current.action, current.state))
+            path.insert(0,
+                        Transition(current.action, current.state))
             current = current.parent
 
-        return self.solution_path
+        return path
 
     def __str__(self):
         return f'Solution path cost: {self.solution_path_cost} \n' \
@@ -36,4 +37,4 @@ class Statistics:
                f'Frontier node count: {self.frontier_count} \n' \
                f'Expanded node count: {self.expanded_count} \n' \
                f'Execution time: {self.execution_time} \n' \
-            f'Solution path: {self.solution_path}'
+            f'Solution path: {[f"{step}" for step in self.solution_path]}'
--- TP1/search_methods/statistics/Transition.py
+++ TP1/search_methods/statistics/Transition.py
@@ -2,3 +2,5 @@ class Transition:
     def __init__(self, parent_action, state):
         self.parent_action = str(parent_action)
         self.state = state
+    def __str__(self) -> str:
+        return f'{self.parent_action} -> {self.state}'
