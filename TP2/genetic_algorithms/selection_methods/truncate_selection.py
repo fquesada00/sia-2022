06c7a9ad1e29a38ad5153d1754f718bfeab5a403
Gonzalo Arca <goarca@itba.edu.ar>
2022-03-31 11:22:51 -0300

    fix :bug: correct truncate selection method

--- TP2/genetic_algorithms/selection_methods/truncate_selection.py
+++ TP2/genetic_algorithms/selection_methods/truncate_selection.py
@@ -1,8 +1,21 @@
 
 import random
 
-def truncate_selection(population, fitness_function, selection_size):
-    
-    # Discard the less fit individuals from the population
-    sorted_population = sorted(population, key=fitness_function, reverse=True)
-    return sorted_population[:selection_size]
+
+def truncate_selection(population, fitness_function, selection_size, k):
+    if (len(population) - k) < selection_size:
+        raise ValueError(
+            "The truncated population size is smaller than the selection size")
+
+    # Discard the less fit k individuals from the population
+    sorted_population = sorted(population, key=fitness_function)
+    truncated_population = sorted_population[k:]
+
+    selection = []
+
+    # Select the selection_size individuals from the truncated population, randomly
+    for _ in range(selection_size):
+        random_index = random.randint(0, len(truncated_population) - 1)
+        selection.append(truncated_population.pop(random_index))
+
+    return selection
