7e56cd756037b9b5d92828698f807129929ba7b2
Gonzalo Arca <goarca@itba.edu.ar>
2022-03-31 22:49:13 -0300

    fix :bug: add extra parameters to optimize method for mutation

--- TP2/genetic_algorithms/__init__.py
+++ TP2/genetic_algorithms/__init__.py
@@ -44,11 +44,12 @@ def evolve(population, fitness_function, select, crossover, mutation_method, mut
     return select(new_population + population, fitness_function, len(population))
 
 
-def optimize(population_size, fitness_function, select, crossover, cut_condition, min, max, with_replacement):
+def optimize(population_size, fitness_function, select, crossover, mutation_method, mutation_rate, cut_condition, min, max):
     population = generate_initial_population(population_size, min, max)
 
     while not cut_condition(population):
-        population = evolve(population, fitness_function, select, crossover)
+        population = evolve(population, fitness_function,
+                            select, crossover, mutation_method, mutation_rate)
         number_of_generations += 1
 
     return population
