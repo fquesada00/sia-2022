027a91fdf308cc0f867acab89cc701b59e23cead
Francisco Quesada <fquesada@itba.edu.ar>
2022-04-01 01:21:25 -0300

    fix :bug: copy population tournament selection

--- TP2/genetic_algorithms/selection_methods/tournament_selection.py
+++ TP2/genetic_algorithms/selection_methods/tournament_selection.py
@@ -1,11 +1,12 @@
 
 import random
 
-def tournament_selection(population, fitness_function, selection_size):
+
+def tournament_selection(population: list, fitness_function, selection_size: int):
 
     selection = []
     threshold = random.random()
-    population_length = len(population)
+    population_copy = population.copy()
     number_of_initial_couples = 2
 
     for _ in range(selection_size):
@@ -16,39 +17,46 @@ def tournament_selection(population, fitness_function, selection_size):
         #     random_index = random.randint(0, len(population) - 1)
         #     couples.append(population[random_index])
 
-
         # Obtain two random couple of individuals from the population and apply tournament algorithm
-        first_individual_index = random.choice(range(len(population)))
-        population.pop(first_individual_index)
+        first_individual_index = random.choice(range(len(population_copy)))
+        population_copy.pop(first_individual_index)
 
-        second_individual_index = random.choice(range(len(population)))
-        population.pop(second_individual_index)
+        second_individual_index = random.choice(range(len(population_copy)))
+        population_copy.pop(second_individual_index)
 
-        third_individual_index = random.choice(range(len(population)))
-        population.pop(third_individual_index)
+        third_individual_index = random.choice(range(len(population_copy)))
+        population_copy.pop(third_individual_index)
 
-        fourth_individual_index = random.choice(range(len(population)))
-        population.pop(fourth_individual_index)
+        fourth_individual_index = random.choice(range(len(population_copy)))
+        population_copy.pop(fourth_individual_index)
 
-        first_couple = [population[first_individual_index], population[second_individual_index]]
-        first_couple_winner_index, first_couple_loser_index = tournament(first_couple, fitness_function, threshold)
+        first_couple = [population_copy[first_individual_index],
+                        population_copy[second_individual_index]]
+        first_couple_winner_index, first_couple_loser_index = tournament(
+            first_couple, fitness_function, threshold)
 
-        second_couple = [population[third_individual_index], population[fourth_individual_index]]
-        second_couple_winner_index, second_couple_loser_index = tournament(second_couple, fitness_function, threshold)
+        second_couple = [population_copy[third_individual_index],
+                         population_copy[fourth_individual_index]]
+        second_couple_winner_index, second_couple_loser_index = tournament(
+            second_couple, fitness_function, threshold)
 
         # Make another couple from the winners of both tournaments
-        third_couple = [first_couple[first_couple_winner_index], second_couple[second_couple_winner_index]]
-        third_couple_winner_index, third_couple_loser_index = tournament(third_couple, fitness_function, threshold)
+        third_couple = [first_couple[first_couple_winner_index],
+                        second_couple[second_couple_winner_index]]
+        third_couple_winner_index, third_couple_loser_index = tournament(
+            third_couple, fitness_function, threshold)
 
         # Add the winner of the third couple to the selection
         selection.append(third_couple[third_couple_winner_index])
 
         # Add the losers of all couples to the population
-        population.extend([first_couple[first_couple_loser_index], second_couple[second_couple_loser_index], third_couple[third_couple_loser_index]])
+        population_copy.extend([first_couple[first_couple_loser_index],
+                               second_couple[second_couple_loser_index], third_couple[third_couple_loser_index]])
 
     return selection
 
-def tournament(couple, fitness_function, threshold):
+
+def tournament(couple: list, fitness_function, threshold: float):
     first_individual = couple[0]
     second_individual = couple[1]
 
