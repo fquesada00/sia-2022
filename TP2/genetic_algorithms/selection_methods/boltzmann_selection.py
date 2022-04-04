194fc1dc7b7d7cb268f47f44e4ffe508d355b5f6
Francisco Quesada <fquesada@itba.edu.ar>
2022-04-04 14:27:38 -0300

    fix :bug: bolztmann temperature referenced before initialization

--- TP2/genetic_algorithms/selection_methods/boltzmann_selection.py
+++ TP2/genetic_algorithms/selection_methods/boltzmann_selection.py
@@ -1,7 +1,7 @@
 import math
 from constants import EXP_RATE, FINAL_TEMPERATURE, INITIAL_TEMPERATURE
 from genetic_algorithms.selection_methods.roulette_selection import roulette_selection
-from genetic_algorithms import number_of_generations
+import genetic_algorithms
 
 
 def temperature_function(number_of_generations, initial_temperature, final_temperature, exp_rate):
@@ -14,10 +14,10 @@ def get_pseudo_fitness_function(fitness_function, temperature, total_pseudo_fitn
 
 def boltzmann_selection(population, fitness_function, selection_size):
     # Calculate the total pseudo fitness of the population
+    temperature = temperature_function(
+        genetic_algorithms.number_of_generations, INITIAL_TEMPERATURE, FINAL_TEMPERATURE, EXP_RATE)
+
     total_fitness = sum(math.exp(fitness_function(j) / temperature)
                         for j in population)
-
-    temperature = temperature_function(
-        number_of_generations, INITIAL_TEMPERATURE, FINAL_TEMPERATURE, EXP_RATE)
 
     return roulette_selection(population, get_pseudo_fitness_function(fitness_function, temperature, total_fitness), selection_size)
