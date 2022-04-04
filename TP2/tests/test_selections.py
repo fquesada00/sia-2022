c744503f9683eadc652ab6022b801506fed94936
Francisco Quesada <fquesada@itba.edu.ar>
2022-04-04 12:57:38 -0300

    test :white_check_mark: rank selection

--- TP2/tests/test_selections.py
+++ TP2/tests/test_selections.py
@@ -1,7 +1,7 @@
 from cmath import log
 import unittest
 from unittest import mock
-from genetic_algorithms.selection_methods import uniform_selection, truncate_selection, tournament_selection, roulette_selection
+from genetic_algorithms.selection_methods import uniform_selection, truncate_selection, tournament_selection, roulette_selection, rank_selection
 
 from models.Chromosome import Chromosome
 
@@ -10,17 +10,17 @@ class TestSelectionMethods(unittest.TestCase):
 
     def setUp(self):
         population = [
-            [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],  # 0 -> 55
-            [2, 3, 4, 5, 6, 7, 8, 9, 10, 11],  # 1 -> 65
-            [3, 4, 5, 6, 7, 8, 9, 10, 11, 12],  # 2 -> 75
-            [4, 5, 6, 7, 8, 9, 10, 11, 12, 13],  # 3 -> 85
-            [5, 6, 7, 8, 9, 10, 11, 12, 13, 14],  # 4 -> 95
-            [6, 7, 8, 9, 10, 11, 12, 13, 14, 15],  # 5 -> 105
-            [7, 8, 9, 10, 11, 12, 13, 14, 15, 16],  # 6 -> 115
-            [8, 9, 10, 11, 12, 13, 14, 15, 16, 17],  # 7 -> 125
-            [9, 10, 11, 12, 13, 14, 15, 16, 17, 18],  # 8 -> 135
-            [10, 11, 12, 13, 14, 15, 16, 17, 18, 19],  # 9 -> 145
-            #                                        total -> 1000
+            [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],  # 0 -> 55 || -> 0.1
+            [2, 3, 4, 5, 6, 7, 8, 9, 10, 11],  # 1 -> 65 || -> 0.2
+            [3, 4, 5, 6, 7, 8, 9, 10, 11, 12],  # 2 -> 75 || -> 0.3
+            [4, 5, 6, 7, 8, 9, 10, 11, 12, 13],  # 3 -> 85 || -> 0.4
+            [5, 6, 7, 8, 9, 10, 11, 12, 13, 14],  # 4 -> 95 || -> 0.5
+            [6, 7, 8, 9, 10, 11, 12, 13, 14, 15],  # 5 -> 105 || -> 0.6
+            [7, 8, 9, 10, 11, 12, 13, 14, 15, 16],  # 6 -> 115 || -> 0.7
+            [8, 9, 10, 11, 12, 13, 14, 15, 16, 17],  # 7 -> 125 || -> 0.8
+            [9, 10, 11, 12, 13, 14, 15, 16, 17, 18],  # 8 -> 135 || -> 0.9
+            [10, 11, 12, 13, 14, 15, 16, 17, 18, 19],  # 9 -> 145 || -> 1.0
+            #                                        total -> 1000 || -> 5.5
         ]
         self.population = population
         self.fitness_function = lambda x: sum(x)
@@ -71,6 +71,13 @@ class TestSelectionMethods(unittest.TestCase):
         selection = roulette_selection(
             self.population, self.fitness_function, 1)
         self.assertEqual(selection, [self.population[6]])
+
+    @mock.patch('random.random')
+    def test_rank(self, random_mock):
+        random_mock.side_effect = [0.019]
+        selection = rank_selection(
+            self.population, self.fitness_function, 1)
+        self.assertEqual(selection, [self.population[1]])
 
 
 if __name__ == '__main__':
