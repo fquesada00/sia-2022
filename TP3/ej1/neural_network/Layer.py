fddbe4f1ff499874a4bfb899f149f180730321df
Gonzalo Arca <goarca@itba.edu.ar>
2022-04-29 02:08:38 -0300

    :sparkles: add linear separation printer and bug fixes
    Co-authored-by: Octavio Serpe <oserpe@itba.edu.ar>
    Co-authored-by: Francisco Quesada <fquesada@itba.edu.ar>

--- TP3/ej1/neural_network/Layer.py
+++ TP3/ej1/neural_network/Layer.py
@@ -8,28 +8,36 @@ class Layer:
         self.size = size
         self.activation_function = ActivationFunction(activation_function)
 
-    def connect_to(self, lower_layer: Layer):
-        self.accum_adjustment = np.zeros(
-            (self.size, lower_layer.size))
-        self.weights = np.random.rand(
-            self.size, lower_layer.size)
-        print(self.weights)
 
-    def propagate_forward(self, inputs, biases):
-        self.excitations = np.dot(self.weights, inputs) + biases
+    def connect_to(self, lower_layer: Layer, bias: float):
+        self.accum_adjustment = np.zeros(
+            (self.size, lower_layer.size + 1))
+
+        self.weights = np.append(np.full((self.size, 1), bias), np.random.uniform(
+            low=-1, high=1, size=(self.size, lower_layer.size)), axis=1)
+        print(f"CONNNECT TO {self.weights}")
+
+    def propagate_forward(self, inputs):
+        self.excitations = np.dot(self.weights, inputs)
         self.activations = self.activation_function(self.excitations)
 
         return self.activations
 
     def propagate_backward(self, upper_weights: np.ndarray, upper_errors: np.ndarray):
+        # Leave out the bias unit from the upper layer's weights (first column)
         self.errors = self.df_activation_function(
-            self.excitations) * np.dot(upper_weights.T, upper_errors)
-
+            self.excitations) * np.dot(upper_weights[:, 1:].T, upper_errors)
+            
         return self.errors
 
-    def update_accum_adjustment(self, lower_activations: np.ndarray, learning_rate: float):
-        self.accum_adjustment += learning_rate * \
-            np.dot(self.errors, lower_activations.T)
+    def update_accum_adjustment(self, lower_activations: np.ndarray, learning_rate: float): 
+        matrix_lower_activations = np.array([lower_activations])
+        accum = learning_rate * \
+            np.dot(self.errors, matrix_lower_activations)
+        if accum[1] < 0:
+            print("negative accum: ",accum)
+        self.accum_adjustment += accum
+
 
     def update_weights(self):
         self.weights += self.accum_adjustment
--- TP3/ej1/neural_network/NeuralNetwork.py
+++ TP3/ej1/neural_network/NeuralNetwork.py
@@ -1,3 +1,4 @@
+from io import FileIO
 import numpy as np
 
 from typing import List
@@ -6,11 +7,12 @@ from neural_network.Layer import Layer
 
 
 class NeuralNetwork:
-    def __init__(self, hidden_sizes: list, input_size: int, output_size: int, learning_rate: float, bias: float, activation_function: str, batch_size=1):
+    def __init__(self, hidden_sizes: list, input_size: int, output_size: int, learning_rate: float, bias: float, activation_function: str, batch_size=1, output_file_name: str=None):
         self.activation_function = activation_function
         self.learning_rate = learning_rate
         self.batch_size = batch_size
         self.bias = bias
+        self.output_file_name = output_file_name
         self.initialize_layers(input_size, output_size, hidden_sizes)
 
     def initialize_layers(self, input_size: int, output_size: int, hidden_sizes: list):
@@ -20,15 +22,17 @@ class NeuralNetwork:
 
         for layer_size in [*hidden_sizes, output_size]:
             layer = Layer(layer_size, self.activation_function)
-            layer.connect_to(self.layers[-1])
+            layer.connect_to(self.layers[-1], self.bias)
             self.layers.append(layer)
 
     def propagate_forward(self, input_data: np.ndarray):
         lower_activation = input_data
 
+        self.layers[0].activations = lower_activation
+
         for layer in self.layers[1:]:
             layer_activation = layer.propagate_forward(
-                lower_activation, np.full((layer.size), -self.bias))
+                lower_activation)
 
             lower_activation = layer_activation
 
@@ -38,6 +42,14 @@ class NeuralNetwork:
         # Start from the output layer
         upper_weights = self.layers[-1].weights
         upper_errors = output_error
+
+        # Set error for output layer
+        self.layers[-1].errors = upper_errors
+
+        # Update weights for output layer
+        self.layers[-1].update_accum_adjustment(
+                self.layers[-2].activations, self.learning_rate)
+        
 
         # Iterate over the hidden layers in reverse order
         for index in reversed(range(1, len(self.layers) - 1)):
@@ -51,10 +63,14 @@ class NeuralNetwork:
             upper_weights = self.layers[index].weights
 
     def update_weights(self):
-        for layer in self.layers:
+        for layer in self.layers[1:]:
             layer.update_weights()
 
     def train(self, input_dataset: np.ndarray, expected_output: np.ndarray):
+        if (self.output_file_name is not None):
+            output_file = open(self.output_file_name, "w")
+            self.save_output_weights_shape(output_file)
+
         batch_iteration = 0
         
         # Calculate min and max values of the expected output for normalization
@@ -65,27 +81,58 @@ class NeuralNetwork:
         output_layer = self.layers[-1]
         to_min = output_layer.activation_function.min()
         to_max = output_layer.activation_function.max()
-
+        
         # Iterate over the input data (the data set)
         for index, input_data in enumerate(input_dataset):
-
-            output = self.propagate_forward(input_data)
+            output = self.predict(input_data)
 
             # Normalize the output
             normalized_output = self.normalize(output, from_min, from_max, to_min, to_max)
-
+            print(f"normalized output - expected output {normalized_output} - {expected_output[index]} = {normalized_output - expected_output[index]}")
             output_error = output_layer.activation_function.derivative(
-                output_layer.excitations) * (normalized_output - output)
-
+                output_layer.excitations) * (normalized_output - expected_output[index])
+                
             self.propagate_backward(output_error)
 
             batch_iteration += 1
-
+            
             if batch_iteration == self.batch_size:
                 # Print weights to file
+                if (self.output_file_name is not None):
+                    self.save_output_weights(output_file)
 
                 self.update_weights()
                 batch_iteration = 0
 
+        self.save_output_weights(output_file)
+
+
+    def predict(self, input_data: np.ndarray):
+        input_data_with_bias = np.insert(input_data, 0, -1)
+        return self.propagate_forward(input_data_with_bias)
+        
+    def test(self, input_dataset, expected_output):
+        for input_data in input_dataset:
+            prediction = self.predict(input_data)
+            print(f'{input_data} -> {prediction}')
+
     def normalize(self, x: np.ndarray, from_min: float, from_max: float, to_min: float, to_max: float):
         return (x - from_min) / (from_max - from_min) * (to_max - to_min) + to_min
+
+    def print_weights(self):
+        for index, layer in enumerate(self.layers[1:]):
+            print(f'Layer #{index + 1}: {layer.weights}')
+    
+    def save_output_weights(self, file: FileIO):
+        newline = '\n'
+        output_weights = self.layers[-1].weights
+        file.write(f'{newline.join([f"{self.neuron_to_string(neuron)}" for neuron in self.layers[-1].weights])}{newline}')
+
+    def save_output_weights_shape(self, file: FileIO):
+        newline = '\n'
+        tab = '\t'
+        output_weights = self.layers[-1].weights
+        file.write(f'{tab.join([str(dim) for dim in output_weights.shape])}{newline}')
+
+    def neuron_to_string(self, neuron: np.ndarray):
+        return '\t'.join([str(weight) for weight in neuron])
