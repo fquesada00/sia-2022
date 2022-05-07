aba9cb94fdc7b2dbc5992154e5f7c71521df8fc1
Francisco Quesada <fquesada@itba.edu.ar>
2022-05-07 16:25:58 -0300

    :bug: xor get class wrong

--- TP3/metrics/confusion_matrix.py
+++ TP3/metrics/confusion_matrix.py
@@ -10,7 +10,7 @@ def get_numbers_class(value):
 
 
 def get_xor_class(value):
-    return 1 if value[0] != value[1] else 0
+    return 1 if value[0] > 0 else 0
 
 
 def get_ej2_class(value):
@@ -27,7 +27,7 @@ def generate_confusion_matrix(expected_output, predicted_output, dataset):
     elif dataset in ['number', 'number_noise']:
         classes = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
         get_class = get_numbers_class
-    elif dataset in ['xor_simple, xor_multilayer', 'and']:
+    elif dataset in ['xor_simple', 'xor_multilayer', 'and']:
         classes = [-1, 1]
         get_class = get_xor_class
     elif dataset in ['ej_2_linear', 'ej_2_non_linear']:
