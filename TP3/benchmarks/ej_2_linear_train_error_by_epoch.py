326504655abafdeb29453575a9f9cf51e5f6ff23
oserpe <oserpe@itba.edu.ar>
2022-05-09 18:16:33 -0300

    :recycle: refactor linear benchmark

--- TP3/benchmarks/ej_2_linear_train_error_by_epoch.py
+++ TP3/benchmarks/ej_2_linear_train_error_by_epoch.py
@@ -7,7 +7,7 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
 
     parser.add_argument("--epochs", help="Number of epochs to train the network.",
-                        dest='epochs', required=False, default='2')
+                        dest='epochs', required=False, default='5')
     parser.add_argument("--iterations", help="Number of iterations per epoch.",
                         dest='iterations', required=False, default='5')
 
@@ -23,13 +23,24 @@ if __name__ == "__main__":
     config_file.close()
 
     epochs = int(args.epochs)
+    batches = [1, 10, 20, 50, 100, 200]
     iterations = int(args.iterations)
-    for iteration in range(iterations):
-        print(f"Iteration {iteration + 1} of {iterations}")
-        for epoch in range(epochs):
-            print(f"Epoch {epoch + 1}")
+    for batch in batches:
+        print(f"Batch size: {batch}")
+
+        config_file = open("TP3/config.json", "r")
+        json_object = json.load(config_file)
+        config_file.close()
+
+        json_object["optimal_parameters"]["ej_2_linear"]["training"]["batch_size"] = batch
+        config_file = open("TP3/config.json", "w")
+        json.dump(json_object, config_file)
+        config_file.close()
+
+        for iteration in range(iterations):
+            print(f"Iteration {iteration + 1} of {iterations}")
             # Execute python script
-            cmd = f"python -m TP3.ej2.main --epochs {epoch + 1} --log-train"
+            cmd = f"python -m TP3.ej2.main --epochs {epochs} --log-train --seed {iteration + 10}"
             print(cmd)
             os.system(cmd)
 
