3994f0e28baeb4aaea471c45da50dacd0ff106d8
Francisco Quesada <fquesada@itba.edu.ar>
2022-05-26 19:02:19 -0300

    :bug: fix bug kohonen getting neighbors

--- TP4/ej1a.py
+++ TP4/ej1a.py
@@ -1,7 +1,7 @@
 from pprint import pprint
 import numpy as np
 
-from models import Kohonen  
+from models.Kohonen import Kohonen
 from utils.read_dataset import read_dataset
 
 
@@ -14,11 +14,22 @@ def main():
     # scale dataset
     data_scaled = (data_no_countries - data_no_countries.mean()
                    ) / data_no_countries.std()
-    k = 7
-    kohonen = Kohonen(k, data_scaled,10,0.9)
-    elems = kohonen.train(data_scaled, 100)
-    neuron_map = kohonen.test(data_scaled)
+    data_scaled_numpy = data_scaled.to_numpy()
+
+    k = 4
+
+    kohonen = Kohonen(k, data_scaled_numpy, 4, 0.01)
+    # pprint(data_scaled_numpy)
+    pprint(kohonen.weights)
+    winner_idx_arr_row, winner_idx_arr_col, radius_arr, learning_rate_arr, dist_arr = kohonen.train(
+        data_scaled_numpy, 1000)
+
+    pprint(kohonen.weights)
+    neuron_map = kohonen.test(data_scaled_numpy)
     pprint(neuron_map)
+    # pprint(kohonen.weights)
+    # pprint(list(zip(winner_idx_arr_row, winner_idx_arr_col)))
+
 
 if __name__ == '__main__':
     main()
--- TP4/models/Kohonen.py
+++ TP4/models/Kohonen.py
@@ -1,44 +1,51 @@
 import numpy as np
+from pandas import array
 
 
 class Kohonen():
 
-    def __create_network(self, k, dataset_input: np.ndarray, ):
-        self.weights = np.array([])
-        for i in range(k):
-            for j in range(k):
-                random_input_index = np.random.choice(
-                    dataset_input.shape[0], size=1)
-                self.weights = np.append(
-                    self.weights, dataset_input[random_input_index, :])
-        self.weights = self.weights.reshape(k, k, dataset_input.shape[1])
-
-    def __init__(self, k, dataset_input,initial_r, initial_lr):
+    def __init__(self, k, dataset_input, initial_r, initial_lr):
         self.k = k
         self.initial_r = initial_r
         self.initial_lr = initial_lr
         self.__create_network(k, dataset_input)
 
+    def __create_network(self, k, dataset_input: np.ndarray, ):
+
+        self.weights = np.array([])
+
+        for i in range(k):
+            for j in range(k):
+                random_input_index = np.random.choice(
+                    dataset_input.shape[0], size=1)[0]
+
+                self.weights = np.append(
+                    self.weights, dataset_input[random_input_index, :])
+
+        self.weights = self.weights.reshape(k, k, dataset_input.shape[1])
+
     def __get_winner(self, input_vector):
         winner_index_i, winner_index_j = 0, 0
-        winner_distance = np.linalg.norm(input_vector-self.weights[0, 0, :])
+        winner_distance = float('inf')
+        o = 0
+        # print("winner distance", winner_distance)
         for i in range(self.k):
             for j in range(self.k):
-                distance = np.linalg.norm(input_vector-self.weights[i, j, :])
+                distance = np.linalg.norm(
+                    input_vector-self.weights[i, j, :], ord=2)
                 if distance < winner_distance:
                     winner_distance = distance
                     winner_index_i = i
                     winner_index_j = j
         return winner_index_i, winner_index_j, self.weights[winner_index_i, winner_index_j, :], winner_distance
 
-
-    def decay_radius(self, time,time_constant):
+    def decay_radius(self, time, time_constant):
         return self.initial_r * np.exp(-time/time_constant)
 
-    def decay_learning_rate(self, time,n_iterations):
-        return self.initial_lr * np.exp(-time/n_iterations)
+    def decay_learning_rate(self, time, epochs):
+        return self.initial_lr * np.exp(-time/epochs)
 
-    def train(self, dataset_input, n_iterations):
+    def train(self, dataset_input, epochs):
 
         winner_idx_arr_row = []
         winner_idx_arr_col = []
@@ -46,47 +53,46 @@ class Kohonen():
         learning_rate_arr = []
         dist_arr = []
 
-        time_constant = n_iterations / np.log(self.initial_r)
+        time_constant = epochs / np.log(self.initial_r)
 
-        for i in range(n_iterations):
+        for i in range(epochs + 1):
 
-            # Choose a random element from the input values
-            random_input_index = np.random.choice(
-                dataset_input.shape[0], size=1)
-            input_vector = dataset_input[random_input_index, :]
+            shuffled_inputs = np.random.permutation(dataset_input)
 
-            # Get the winner index
-            winner_index_i, winner_index_j,winner,dist = self.__get_winner(
-                input_vector)
+            for input_vector in shuffled_inputs:
+                # Get the winner index
+                winner_index_i, winner_index_j, winner, dist = self.__get_winner(
+                    input_vector)
 
-            winner_idx_arr_row.append(winner_index_i)
-            winner_idx_arr_col.append(winner_index_j)
-            dist_arr.append(dist)
+                winner_idx_arr_row.append(winner_index_i)
+                winner_idx_arr_col.append(winner_index_j)
+                dist_arr.append(dist)
 
-            # Decay radius and learning rate
+                # Decay radius and learning rate
 
-            radius = self.decay_radius(i, time_constant)
-            lr = self.decay_learning_rate(i, n_iterations)
+                radius = self.decay_radius(i, time_constant)
+                lr = self.decay_learning_rate(i, epochs)
 
-            radius_arr.append(radius)
-            learning_rate_arr.append(lr)
+                radius_arr.append(radius)
+                learning_rate_arr.append(lr)
 
-            # Update the winner neighborhood
-            for x in range(self.k):
-              for y in range(self.k):
-                w = self.weights[x, y, :]
+                # Update the winner neighborhood
+                for x in range(self.k):
+                    for y in range(self.k):
+                        w = self.weights[x, y, :]
 
-                w_dist = np.linalg.norm(w-winner)
+                        neuron_distance = np.linalg.norm(
+                            np.array([winner_index_i-x, winner_index_j-y]))
 
-                if w_dist <= radius:
-                    self.weights[x, y, :] = w + lr*(input_vector-w)
+                        if neuron_distance <= radius:
+                            self.weights[x, y, :] = w + lr*(input_vector-w)
 
         return winner_idx_arr_row, winner_idx_arr_col, radius_arr, learning_rate_arr, dist_arr
 
-    def test(self,dataset_input):
-      neurons = np.zeros(shape=(self.k,self.k))
-      for input_value in dataset_input:
-        winner_index_i, winner_index_j,winner,dist = self.__get_winner(input_value)
-        neurons[winner_index_i,winner_index_j] += 1
-      return neurons
-
+    def test(self, dataset_input):
+        neurons = np.zeros(shape=(self.k, self.k))
+        for input_value in dataset_input:
+            winner_index_i, winner_index_j, winner, dist = self.__get_winner(
+                input_value)
+            neurons[winner_index_i, winner_index_j] += 1
+        return neurons
